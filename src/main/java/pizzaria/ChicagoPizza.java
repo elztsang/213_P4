5f0af319e4cd7dc548adcd722cd6f8f56b07d14f
rona <breamstatue@gmail.com>
2024-11-18 13:54:42 -0500

    added documentation for the 2 factory classes

--- src/main/java/pizzaria/ChicagoPizza.java
+++ src/main/java/pizzaria/ChicagoPizza.java
@@ -6,21 +6,37 @@ package pizzaria;
  * @author Elizabeth Tsang, Ron Chrysler Amistad
  */
 public class ChicagoPizza implements PizzaFactory{
+    /** Creates a Deluxe pizza, Chicago-style.
+     *
+     * @return Deluxe pizza
+     */
     @Override
     public Pizza createDeluxe() {
         return new Deluxe(Crust.DEEPDISH);
     }
 
+    /** Creates a Meatzza pizza, Chicago-style.
+     *
+     * @return Meatzza pizza
+     */
     @Override
     public Pizza createMeatzza() {
         return new Meatzza(Crust.STUFFED);
     }
 
+    /** Creates a BBQChicken pizza, Chicago-style.
+     *
+     * @return BBQChicken pizza
+     */
     @Override
     public Pizza createBBQChicken() {
         return new BBQChicken(Crust.PAN);
     }
 
+    /** Creates a BYO pizza, Chicago-style.
+     *
+     * @return BYO pizza
+     */
     @Override
     public Pizza createBuildYourOwn() {
         return new BuildYourOwn(Crust.PAN);
--- src/main/java/pizzaria/NYPizza.java
+++ src/main/java/pizzaria/NYPizza.java
@@ -6,21 +6,37 @@ package pizzaria;
  * @author Elizabeth Tsang, Ron Chrysler Amistad
  */
 public class NYPizza implements PizzaFactory {
+    /** Creates a Deluxe pizza, NY-style.
+     *
+     * @return Deluxe pizza
+     */
     @Override
     public Pizza createDeluxe() {
         return new Deluxe(Crust.BROOKLYN);
     }
 
+    /** Creates a Meatzza pizza, NY-style.
+     *
+     * @return Meatzza pizza
+     */
     @Override
     public Pizza createMeatzza() {
         return new Meatzza(Crust.HANDTOSSED);
     }
 
+    /** Creates a BBQChicken pizza, NY-style.
+     *
+     * @return BBQChicken pizza
+     */
     @Override
     public Pizza createBBQChicken() {
         return new BBQChicken(Crust.THIN);
     }
 
+    /** Creates a BYO pizza, NY-style.
+     *
+     * @return BYO pizza
+     */
     @Override
     public Pizza createBuildYourOwn() {
         return new BuildYourOwn(Crust.HANDTOSSED);
